adaded6b7f5e2556e5ffd8a2c356509722ef39fb
Blackbear26 <163040073+Blackbear26@users.noreply.github.com>
2024-11-27 12:52:03 -0600

    Asignamos y eliminamos styles con JS

--- 02 estilos/script.js
+++ 02 estilos/script.js
@@ -40,4 +40,66 @@ a diferencia de CCS las propiedades de estilos en JS las escribimos usando el fo
 
 */
 
-console.log("valores en leinea. color: " + titulo.style.color + " - fondo: " + titulo.style.backgroundColor)
+console.log("valores en leinea. color: " + titulo.style.color + " - fondo: " + titulo.style.backgroundColor);
+
+/* 
+ mwtodo de propiedad
+
+ Asignar el valor de la propiedad del estilo en el elmento seleccionado
+
+    elemento.style.nombrePropiedad = "valor"
+
+-Es la forma mas directa y legible de asignar propiedades 
+-Solo permite asignar proopiedades en camelcase
+ */
+
+titulo.style.color = "purple";
+titulo.style.backgroundColor = "yellow";
+
+console.log("valores asignados con JS. color: " + titulo.style.color + " - fondo: " + titulo.style.backgroundColor);
+
+/* 
+metodo setProperty()
+
+Asignamos una propiedad de estilo al elemento seleccionado 
+
+    elemento.style.setProperty(nombreDeLaPropiedad, valor, important)
+
+-MAs flexible y poderoso
+-se escriben en formato kebab-case
+-parametro important es opcional
+
+*/
+
+titulo.style.setProperty("color", "darkblue");
+titulo.style.setProperty("backgroundColor", "red", "important");
+console.log("Asignados con Property. color: " + titulo.style.color + " - fondo: " + titulo.style.backgroundColor);
+
+/* 
+eleminar  varolores de propiedades 
+
+usamos el metodo de propiedad y le asignamos una cadena de texto bacia
+
+    elemento.style.nombrePropiedad = ""
+
+esto elimina los valores previamente definidos en JS y en los styles en linea
+
+*/
+
+/* titulo.style.color = ""
+titulo.style.backgroundColor = "" */
+
+/* 
+metodo removeProprety 
+ 
+elmina la propiedad de stilo
+
+    elemento.style.romeveProprety
+    (nombreP¨ropiedad)
+
+ -las propiedades se pasan en formato kebat-case
+*/
+
+
+titulo.style.removeProperty("color");
+titulo.style.removeProperty("background-color");
