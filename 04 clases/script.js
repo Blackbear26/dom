1a4c0551a182865ebee14eabd26a3a3996ce7b73
Blackbear26 <163040073+Blackbear26@users.noreply.github.com>
2024-12-04 15:52:20 -0600

    terminamos con clase JS

--- 04 clases/script.js
+++ 04 clases/script.js
@@ -2,6 +2,48 @@
 Aceder al primer elemento de la lista
 */
 
-const primerBotana = document.getElementsByTagName("li");
+const botanas = document.getElementsByTagName("li");
 
-console.log(primerBotana[0]);
+/* Propiedad clss list 
+
+accedemos a la lista de clases que tiene un elemento  
+
+    eleemento.classList
+
+-devueleve un token list (listado de clases en el DOM) en tipo array
+*/
+
+console.log(botanas[0].classList[1]);
+
+/* para agreagr clases uutilizamos el metodo add
+
+    elemento.classList.add(clase)
+
+- nombr de la clase pasa como string entre " "
+*/
+
+botanas[2].classList.add("bg-lightpink");
+
+/* 
+Verificar si existe una clase en ele lemento con el metodo contains
+
+    elemento.classList.contains(clase)
+
+- devuelve true si la clase existe en el elemento
+- devuelve false si la clase no existe en el elemento
+- la clase pasa como string ("")
+*/
+
+console.log(botanas[2].classList.contains("botana"));
+console.log(botanas[2].classList.contains("bg-lightblue"));
+
+/* 
+Para eliminar una clase con el metodo remove
+
+    elemento.classList.remove(clase)
+
+- la clase pasa como string ("")
+*/
+
+botanas[3].classList.remove("bg-lightpink");
+botanas[3].classList.remove("botana");
