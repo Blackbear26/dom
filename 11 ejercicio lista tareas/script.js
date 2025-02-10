c441b21b94892f24672589233f063b6c4a995c8f
Blackbear26 <163040073+Blackbear26@users.noreply.github.com>
2025-02-10 17:10:14 -0600

    funcionalidad de agregar tarea

--- 11 ejercicio lista tareas/script.js
+++ 11 ejercicio lista tareas/script.js
@@ -1,6 +1,7 @@
-//referencias
+//referencia
 const taskInput = document.getElementById('taskInput');
 const addButton = document.getElementById('addButton');
+const taskList = document.getElementById('taskList');
 
 // escuchadora
 
@@ -11,6 +12,32 @@ addButton.addEventListener('click', createTask);
 function createTask () {
     if (taskInput.value) {
         console.log(taskInput.value);
+
+        /* creamos el contenedor de una tarea */
+        const taskItem = document.createElement('div');
+        taskItem.classList.add('task')
+
+        /* creamos el parrafo de la tarea */
+        const taskText = document.createElement('p')
+        taskText.innerText = taskInput.value;
+
+        /* creamos el contenedor de los iconos */
+        const taskIcon = document.createElement('div');
+        taskIcon.classList.add('task-icon');
+
+        /* creamos el icono de check */
+        const checkIcon = document.createElement('i');
+        checkIcon.classList.add('bi', 'bi-check', 'icon-complete');
+
+        /* ceamos el icono de delate */
+        const deleteIcon = document.createElement('i');
+        deleteIcon.classList.add('bi', 'bi-trash', 'icon-delete');
+
+        /* estructura de los elementos  */
+        taskIcon.append(checkIcon, deleteIcon);
+        taskItem.append(taskText, taskIcon);
+        taskList.append(taskItem);
+
         taskInput.value = '';
     } else {
         alert('Escribe una tarea');
